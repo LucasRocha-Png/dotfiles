539275cb1c892e3d31374b20c202023cd590a37c
Lucas <lucasrocha.png@gmail.com>
2026-05-03 16:23:49 -0300

    Corrige bug roll name

--- hacker/polybar/salieri/scripts/roll_name.sh
+++ hacker/polybar/salieri/scripts/roll_name.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-pkill zscroll
+pkill -x zscroll
 
-zscroll -l 20 \
-        --delay 0.8 \
-        --match-command "playerctl status 2>/dev/null" \
-        --match-text "Playing" "--scroll 1" \
-        --match-text "Paused" "--scroll 0" \
-        --update-check true "$HOME/dotfiles/hacker/polybar/salieri/scripts/get_media.sh" &
+while true; do
+    MEDIA=$("$HOME/dotfiles/hacker/polybar/salieri/scripts/get_media.sh")
 
-wait
+    if [ -n "$MEDIA" ]; then
+        if ! pgrep -x "zscroll" > /dev/null; then
+            zscroll -l 20 \
+                    --delay 0.8 \
+                    --match-command "playerctl status 2>/dev/null || echo 'Stopped'" \
+                    --match-text "Playing" "--scroll 1" \
+                    --match-text "Paused" "--scroll 0" \
+                    --match-text "Stopped" "--scroll 0" \
+                    --update-check true "$HOME/dotfiles/hacker/polybar/salieri/scripts/get_media.sh" &
+        fi
+    else
+        if pgrep -x "zscroll" > /dev/null; then
+            pkill -x zscroll
+        fi
+        echo ""
+    fi
+    
+    sleep 2
+done
